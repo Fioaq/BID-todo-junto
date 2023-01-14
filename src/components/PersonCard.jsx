33730d298dc89dff7182f8b811c9097ba6e82d4f
Fiorella Aquino <fiorellaaquino05@gmail.com>
2023-01-14 19:23:04 -0300

    primer commit

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,21 @@
-import { Component } from "react";
+import React,{ Component } from "react";
 
 class PersonCard extends Component {
-    render() { 
-        const {firstName, lastName, age, hairColor}= this.props;
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: parseInt(this.props.age)
+        };
+    }
+
+    render() {
+        const {firstName, lastName, hairColor}= this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={() => this.setState({age: this.state.age + 1})}>Birthday Button for {firstName} {lastName}</button>
             </div>
         );
     }
